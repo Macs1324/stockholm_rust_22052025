c0a06a5bc90c19231984440578d0d67b7d0e474d
Macs1324 <max.blank410@gmail.com>
2025-05-19 14:10:06 +0200

    first component

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,10 +1,23 @@
 use bevy::{color::palettes::tailwind::CYAN_800, prelude::*};
 
+const GRID_X: f32 = 30.0;
+const GRID_Y: f32 = 30.0;
+const GRID_Z: f32 = 30.0;
+
+const CELL_SIZE: f32 = 0.1;
+const CELL_GAP: f32 = 0.01;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .run();
+}
+
+#[derive(Component)]
+enum Cell {
+    Alive,
+    Dead,
 }
 
 pub fn setup(
@@ -26,13 +39,29 @@ pub fn setup(
         Transform::from_xyz(3.0, 8.0, 5.0),
     ));
 
-    commands.spawn((
-        Name::new("Cube"),
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
-        Transform::from_xyz(0.0, 0.0, 0.0),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::srgb(0.5, 1.0, 0.6),
-            ..default()
-        })),
-    ));
+    for i in 0..GRID_X as i32 {
+        for j in 0..GRID_Y as i32 {
+            for k in 0..GRID_Z as i32 {
+                commands.spawn((
+                    Name::new("Cell"),
+                    Cell::Alive,
+                    Visibility::Visible,
+                    Mesh3d(meshes.add(Cuboid::new(
+                        CELL_SIZE - CELL_GAP,
+                        CELL_SIZE - CELL_GAP,
+                        CELL_SIZE - CELL_GAP,
+                    ))),
+                    Transform::from_xyz(
+                        i as f32 * CELL_SIZE,
+                        j as f32 * CELL_SIZE,
+                        k as f32 * CELL_SIZE,
+                    ),
+                    MeshMaterial3d(materials.add(StandardMaterial {
+                        base_color: Color::srgb(0.5, 1.0, 0.6),
+                        ..default()
+                    })),
+                ));
+            }
+        }
+    }
 }
